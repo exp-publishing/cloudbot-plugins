e0f22ccea6710fc0a3c2bf02ae31aafb318d360e
Josh Elsasser <josh@elsasser.ca>
2016-07-03 22:48:47 -0700

    actually run the bot

--- test/conftest.py
+++ test/conftest.py
@@ -48,13 +48,14 @@ def server(request):
     return server
 
 
-@pytest.yield_fixture(scope='session')
+@pytest.fixture(scope='session')
 def bot(request, server):
     """Return a CloudBot instance connected to a local irc server
     """
     def run_bot(loop):
         asyncio.set_event_loop(loop)
 
+        paths.add_plugin_dir('plugins')
         paths.set_path('config_file', 'test/conf.test.json')
         paths.set_path('log_dir', 'test/logs')
         paths.set_path('data_dir', 'test/data')
@@ -62,6 +63,7 @@ def bot(request, server):
         setup_bot()
 
         bot = CloudBot(loop)
+        bot.run()
 
     thread = threading.Thread(target=run_bot, args=(asyncio.get_event_loop(),))
     thread.daemon = True
@@ -70,5 +72,3 @@ def bot(request, server):
     def fin():
         thread.join(5)
     request.addfinalizer(fin)
-
-    yield bot
